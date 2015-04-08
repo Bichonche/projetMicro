9129aaa793b034ec27d86c4d75e8b1166a510c80
genietchampion <romain;geniet@laposte.net>
2015-04-08 15:49:40 +0200

    Update detection.h

--- romain/detection.h
+++ romain/detection.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "lpc17xx_pinsel.h"
+#include "declarations.h"
+#include "lpc17xx_adc.h"
+//#include "servoMoteur.h"
+//#include "affichage.h"
 uint16_t chargement (void);
 float distance (void);
 int arret_obligatoire (float d);
