75f2eaf74e38333a1e903072d2634b4cdecf7cf1
genietchampion <romain;geniet@laposte.net>
2015-04-08 15:51:44 +0200

    Update obstacle.c

--- romain/obstacle.c
+++ romain/obstacle.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "lpc17xx_pinsel.h"
+#include "declarations.h"
+#include "lpc17xx_adc.h"
+//#include "servoMoteur.h"
+//#include "affichage.h"
+    typedef enum {
+            vrai  = (1==1),
+            faux = (1==0),
+    } boolean;
+		
 GPIO_Registres *pGPIO0; // Pointeur sur la structure du GPIO0
 GPIO_Registres *pGPIO1; // Pointeur sur la structure du GPIO1
+
 uint16_t chargement (void)
 {
 uint16_t v;
 // On charge les données en entrée sur le GPIO sur le P0.23 dans la valeur v
-(PINSEL1) >> 14 = 1;
-(PINSEL1) >> 15 = 0;
 // On se place en mode AD0.0
 // recopie de p0.23 dans v
-v = ADC_ChannelGetData(ADC0.0,);
+v = ADC_ChannelGetData(LPC_ADC,0);
 // On renvoie la valeur du voltage en fonction de la luminosité
 return v;
 }
 float distance (void)
 {
 float d = 0;
-uint16 v = chargement; // chargement des données du laser
+uint16_t v = chargement(); // chargement des données du laser
 d = v / 0.26; // inversion de la fonction
 return d;
 }
@@ -44,11 +50,10 @@ return a;
 }
 void arreturgence (int a)
 {
-if (a = 2)
-then
+if (a == 2)
 {
-stop();
-AfficheEcran(0,0,"reculez");
+//stop();
+//AfficheEcran(0,0,"reculez");
 }
 else
 {
@@ -57,25 +62,23 @@ else
 boolean obstacle_cote (void)
 {
 boolean b;
-int d1;
-int d2;
 //chargemet des données des microrupteurs depuis P1.11 et P1.21
-d1 = (PGPIO1 -> FIOPIN) >> 8; // copie de p1.11 sur d1 (si 1 le bouton est poussé)
-d2 = (PGPIO1 -> FIOPIN) >> 14;// copie de p1.21 sur d2 (si 1 le bouton est poussé)
+	PINSEL_PIN_8 << 1;	// copie de p1.11 sur d1 (si 1 le bouton est poussé)
+PINSEL_PIN_14 <<1;// copie de p1.21 sur d2 (si 1 le bouton est poussé)
 // Si un des deux boutons est poussé, on envoie un booléen true. Sinon on renvoie false.
-if (d1 /= 0) then
+if (PINSEL_PIN_14 != 0)
 {
-b = true;
+b = vrai;
 }
 else
 {
-if (d2 /= 0) then
+if (PINSEL_PIN_14 != 0)
 {
-b = true;
+b = vrai;
 }
 else
 {
-b = false;
+b = faux;
 }
 }
 return b;
@@ -88,11 +91,7 @@ float d = distance ();
 boolean b = obstacle_cote ();
 arreturgence (arret_obligatoire (d));
 if (b)
-then
 {
-AfficheEcran(0,0,"Obstacle sur le côté");
-}
-else
-{
+//AfficheEcran(0,0,"Obstacle sur le côté");
 }
 }
