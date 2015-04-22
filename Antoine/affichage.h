5dd9ad4e8fe21612a9ba2eaa0b575f6e6762b0fc
aperquin <antoine.perquin@gmail.com>
2015-04-22 12:08:05 +0200

    Update affichage.h

--- Antoine/affichage.h
+++ Antoine/affichage.h
@@ -1,3 +1,11 @@
-void initScreen();
-void printScreen(char * message, char posX, char posY);
-void clearScreen();
+#include "lpc17xx_uart.h"
+#include "lpc17xx_pinsel.h"
+
+//Initalise l'écran et la liaison UART, prend 20 ms.
+void initScreen(void);
+
+//Affiche une chaîne de caractère à la position voulue.
+void printScreen(uint8_t * message, uint8_t posX, uint8_t posY);
+
+//Efface l'écran.
+void clearScreen(void);
