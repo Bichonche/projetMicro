1d0d25d53534fc491270bbf8389ccec7cdbad1cc
genietchampion <romain;geniet@laposte.net>
2015-05-06 14:22:50 +0200

    Update detection.h

--- romain/detection.h
+++ romain/detection.h
@@ -4,11 +4,8 @@
 #include "lpc17xx_pinsel.h"
 #include "declarations.h"
 #include "lpc17xx_adc.h"
-//#include "servoMoteur.h"
-//#include "affichage.h"
 uint16_t chargement (void);
 float distance (void);
 int arret_obligatoire (float d);
-void arreturgence (int a);
 boolean obstacle_cote (void);
-void detection (void);
+int detection (void);
