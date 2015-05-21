2536f871e1b8417c243070fbf9c4f74796691d03
captain67 <etienne.scholly@free.fr>
2015-05-21 15:45:06 +0200

    MAJ reception.c
    qui fonctionne maggle

--- Etienne/reception_ir.c
+++ Etienne/reception_ir.c
@@ -1,104 +1,92 @@
+
 #include "reception.h"
 
 void init(void)
 {
 	
 	PINSEL_CFG_Type PinCfgStruct;
+	TIM_TIMERCFG_Type TimerCfgStruct;
 	TIM_CAPTURECFG_Type CaptureCfgStruct;
-
-	// initialisation des variables
-	flag_debut_transmission = 0;
-	flag_fin_transmission = 0;
-	flag_fin_enregistrement = 0;
-	flag_reception_bit = 0;
 	
-	// configuration du pinsel
-	PinCfgStruct.Portnum = PINSEL_PORT_0;
-	PinCfgStruct.Pinnum = PINSEL_PIN_24;
-	PinCfgStruct.Funcnum = PINSEL_FUNC_3;			//CAP3[1]
-	PinCfgStruct.Pinmode = PINSEL_PINMODE_PULLUP;
-	PinCfgStruct.OpenDrain = PINSEL_PINMODE_NORMAL;
-	PINSEL_ConfigPin(&PinCfgStruct);
+	flag_debut_reception = 0;
+	flag_fin_reception = 0;
+	cpt_bit = 7;
+	cpt_oct = 0;
 	
-	// initialisation du timer en mode capture 
-	CaptureCfgStruct.CaptureChannel = 1;		//channel 1
+	PinCfgStruct.Portnum = PINSEL_PORT_0;													// Port 0.24
+	PinCfgStruct.Pinnum = PINSEL_PIN_24;													// Port 0.24
+	PinCfgStruct.Funcnum = PINSEL_FUNC_3;													// CAP3[1]
+	PINSEL_ConfigPin(&PinCfgStruct);															// Configuration terminée
+	
+	TimerCfgStruct.PrescaleOption = TIM_PRESCALE_USVAL; 					// microsec
+	TimerCfgStruct.PrescaleValue = 1;															// echelle : 1 µs
+	CaptureCfgStruct.CaptureChannel = 1;
 	CaptureCfgStruct.FallingEdge = ENABLE;
-	CaptureCfgStruct.IntOnCaption = DISABLE;
-	CaptureCfgStruct.RisingEdge = ENABLE;			// sur les deux fronts
+	CaptureCfgStruct.RisingEdge = ENABLE;
+	CaptureCfgStruct.IntOnCaption = ENABLE;
+	// CounterCfgStruct.CounterOption = TIM_COUNTER_INCAP1;
+	TIM_Init(LPC_TIM3, TIM_TIMER_MODE, &TimerCfgStruct);					// initialisation du timer
 	TIM_ConfigCapture(LPC_TIM3, &CaptureCfgStruct);
 	
-}
-
-
-/* Interruption du timer ---------------------------------------------------------- */
-
-void TIMER0_IRQHandler()
-{
+	NVIC_EnableIRQ(TIMER3_IRQn);
+	TIM_Cmd(LPC_TIM3, ENABLE);
 	
 }
 
 
-/* Capture du bit courant ----------------------------------------------------------- */
-
-bool capture_duree(int duree)
+void TIMER3_IRQHandler(void)
 {
 	
-	// définition des intervalles de vérification des valeurs temporelles
-	float valeur_stop_min = valeur_stop * marge_basse;
-	float valeur_stop_max = marge_haute * valeur_stop;
-	float valeur_bit1_min = valeur_bit1 * marge_basse;
-	float valeur_bit1_max = valeur_bit1 * marge_haute;
-	float valeur_bit0_min = valeur_bit0 * marge_basse;
-	float valeur_bit0_max =  valeur_bit0 * marge_haute;
-	float valeur_header_min = valeur_header * marge_basse;
-	float valeur_header_max = valeur_header * marge_haute;
+	float head_min = head * marge_basse;
+	float head_max = head * marge_haute;
+	float bit0_min = bit0 * marge_basse;
+	float bit0_max = bit0 * marge_haute;
+	float bit1_min = bit1 * marge_basse;
+	float bit1_max = bit1 * marge_haute;
+	float stop_min = stop * marge_basse;
+	float stop_max = stop * marge_haute;
+
+	duree = TIM_GetCaptureValue(LPC_TIM3, TIM_COUNTER_INCAP1);				// on récup la valeur du timer
 	
-	while (cpt >= 0)
+	if (cpt_bit < 0)
 	{
+		cpt_oct++;
+		cpt_bit = 7;
+	}
+	
+	if (duree > head_min && duree < head_max)					// debut de la transmission
+	{
+		flag_debut_reception = 1;														// la réception a commencé
+	}
 		
-		duree = TIM_GetCaptureValue(LPC_TIM3, TIM_COUNTER_INCAP1);
+	else if (duree > bit1_min && duree < bit1_max)		// le bit vaut 1
+	{
+		Tab[cpt_oct] |= 1 << cpt_bit;																					// on met un 1 dans le tableau
+		cpt_bit--;																							// décrémentation du compteur
+	}
 		
-		if (duree > valeur_header_min && duree < valeur_header_max)		// debut de la transmission
-		{
-			flag_debut_transmission = 1;
-		}
-		
-		else if (duree > valeur_bit1_min && duree < valeur_bit1_max)		// le bit vaut 1
-		{
-			flag_reception_bit = 1;
-			current_bit = 1;
-			T[cpt] = current_bit;			// on le stocke dans le tableau
-			cpt--;
-		}
-		
-		else if (duree > valeur_bit0_min && duree < valeur_bit0_max)		// le bit vaut 0
-		{
-			flag_reception_bit = 1;
-			current_bit = 0;
-			T[cpt] = current_bit;			// on le stocke dans le tableau
-			cpt--;
-		}
-		
-		else if (duree > valeur_stop_min && duree < valeur_stop_max)		// fin de la transmission
-		{
-			flag_fin_transmission = 1;
-		}
-		
-		else		// il ne se passe rien -> on se cale à nouveau 
-		{
-			init();
-		}
+	else if (duree > bit0_min && duree < bit0_max)		// le bit vaut 0
+	{
+		Tab[cpt_oct] &= ~(1 << cpt_bit);																					// on met un 0 dans le tableau
+		cpt_bit--;																							// décrémentation du compteur
+	}
+	
+	else
+	{
+		flag_debut_reception = 0;														// on remet le flag de début à 0
+		flag_fin_reception = 1;															// on passe le flag de fin à 1 (pour lancer décodage)
 	}
 	
 	TIM_ResetCounter(LPC_TIM3);
-	
-	return current_bit;	
+	TIM_ClearIntPending(LPC_TIM3, TIM_CR1_INT);
 	
 }
 
+
 /* Decodage du message ------------------------------------------------------------------- */
 
-unsigned int decodage (bool T[128])
+void decodage (void)
 {
 	
+
 }
