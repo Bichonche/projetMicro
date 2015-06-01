ac8345bd564b04e29719d3baf9016f9595c53f9d
genietchampion <romain.geniet@laposte.net>
2015-06-01 11:55:11 +0200

    Update detection.h

--- romain/detection.h
+++ romain/detection.h
@@ -6,8 +6,7 @@
 #include "lpc17xx_adc.h"
 uint16_t chargement (void);
 float distance (void);
-bool obstacle_cote (void);
+void init(void);
 int detection (void);
 void EINT0_IRQHandler (void);
 void EINT1_IRQHandler (void);
-void init (void);
