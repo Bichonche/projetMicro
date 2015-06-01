ff457834faffb700a403a1480ec7f23a32a34cc7
genietchampion <romain.geniet@laposte.net>
2015-06-01 11:40:59 +0200

    Update detection.c

--- romain/detection.c
+++ romain/detection.c
@@ -7,6 +7,8 @@ GPIO_Registres *pGPIO1; // Pointeur sur la structure du GPIO1
 unsigned int FIO1PIN;
 bool collision_gauche;
 bool collision_droit;
+int d1;
+int d2;
 
 uint16_t chargement (void)
 	{
@@ -14,7 +16,7 @@ uint16_t chargement (void)
 // On charge les données en entrée sur le GPIO sur le P0.23 dans la valeur v
 // On se place en mode AD0.0
 // recopie de p0.23 dans v
-		while(ADC_ChannelGetStatus(LPC_ADC,0,1) != 1)
+		while(!(ADC_ChannelGetStatus(LPC_ADC,0, 1 )))
 			{
 				v = ADC_ChannelGetData(LPC_ADC,0);
 			}
@@ -30,37 +32,27 @@ d = 0.26 / v; // inversion de la fonction
 return d;
 }
 
-bool obstacle_cote (void)
-	{
-bool b;
-int d1;
-int d2;
-//chargemet des données des microrupteurs depuis P1.11 et P1.21
-d1 = (FIO1PIN >>11) & 0x01; // copie de p1.11 sur d1 (si 1 le bouton est poussé)
-d2 = (FIO1PIN >>21) & 0x01;// copie de p1.21 sur d2 (si 1 le bouton est poussé)
-// Si un des deux boutons est poussé, on envoie un booléen true. Sinon on renvoie false.
-
-	if (d1)
-	{
-		EINT0_IRQHandler();
-	}
- else
- {
-	 if (d2)
-	 {
-		 EINT1_IRQHandler();
-	 }
- }
+void init (void)
+{
+	d1 = (FIO1PIN >>11) & 0x01; // copie de p1.11 sur d1 (si 1 le bouton est poussé);
+	d2 = (FIO1PIN >>21) & 0x01;
 }
 
 void EINT0_IRQHandler (void)
 	{
+	if (d1)
+		{
     collision_gauche = true;
+		}
 	}
 
 void EINT1_IRQHandler (void)
 	{
+
+		if (d2)
+		{
 		collision_droit = true;
+		}
 	}
 
 int arret_obligatoire (float d)
@@ -90,9 +82,4 @@ int detection (void)
 /* On mesure la distance et on vérifie qu'aucun obstacle ne se trouve sur le côté*/
 float d = distance ();
 return arret_obligatoire(d);
-}
-
-void detectionglobale (void)
-{
-	obstacle_cote();
 }
